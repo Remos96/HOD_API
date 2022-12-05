e246c197db0d2b906e8c0b48e592abfa4a76e319
Remos96 <80366356+Remos96@users.noreply.github.com>
2022-12-05 18:37:42 -0500

    removed comments

--- MyFirstAPI/Models/DbContext.cs
+++ MyFirstAPI/Models/DbContext.cs
@@ -26,35 +26,3 @@ namespace MyFirstAPI.Models
     }
 }
 
-
-
-
-
-//using Microsoft.EntityFrameworkCore;
-//using System.Diagnostics.CodeAnalysis;
-//using MyFirstAPI.Models;
-
-//namespace MyFirstAPI.Models
-//{
-//    public class MyFirstAPIDBContext : DbContext
-//    {
-//        protected readonly IConfiguration Configuration;
-
-//        public MyFirstAPIDBContext(DbContextOptions<MyFirstAPIDBContext> options, IConfiguration configuration)
-//            : base(options)
-//        {
-//            Configuration = configuration;
-//        }
-
-//        protected override void OnConfiguring(DbContextOptionsBuilder options)
-//        {
-//            var connectionString = Configuration.GetConnectionString("CustomerDataService");
-//            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-//        }
-
-//        //vector<customer> 
-//        public DbSet<Customer> Customers { get; set; } = null!;
-//        public DbSet<Email> Emails { get; set; } = null!;
-//    }
-//}
-
