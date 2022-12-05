03905a404e7220f474c1624e81df218356be02d5
Remos96 <80366356+Remos96@users.noreply.github.com>
2022-12-05 18:37:24 -0500

    Removed comments

--- MyFirstAPI/Models/House.cs
+++ MyFirstAPI/Models/House.cs
@@ -8,14 +8,3 @@ namespace MyFirstAPI.Models
         public string Slogan { get; set; } = null!;
     }
 }
-
-//using System;
-//namespace MyFirstAPI.Models
-//{
-//    public class Email
-//    {
-//        public int EmailId { get; set; }
-//        public string EmailAddress { get; set; }
-//        public bool IsSubscribed { get; set; }
-//    }
-//}
