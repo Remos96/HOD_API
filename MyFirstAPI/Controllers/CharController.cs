e528499fcec902c64b43b2db15181347c111d85a
Remos96 <80366356+Remos96@users.noreply.github.com>
2022-12-05 18:35:06 -0500

    Added StatusCode for post/delete
    also removed filler comments

--- MyFirstAPI/Controllers/CharController.cs
+++ MyFirstAPI/Controllers/CharController.cs
@@ -24,11 +24,9 @@ namespace MyFirstAPI.Controllers
         [HttpGet("/api/character")]
         public async Task<ActionResult<IEnumerable<Character>>> GetCharacters()
         {
-            //if (_context.Characters == null)
-            //{
-            //    return NotFound();
-            //}
-            //return await _context.Characters.ToListAsync();
+            Response responseObject = new Response();
+            responseObject.statusCode = 200;
+            responseObject.statusDescription = "Character list";
             return Ok(await _context.Characters.ToListAsync());
         }
 
@@ -36,28 +34,22 @@ namespace MyFirstAPI.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Character>> GetCharacter(int id)
         {
-            if (_context.Characters == null)
+            Response responseObject = new Response();
+            var characterList = await _context.Characters.FindAsync(id);
+            if (characterList == null)
             {
-                return NotFound();
-            }
-            var character = await _context.Characters.FindAsync(id);
-
-            if (character == null)
-            {
-                return NotFound();
+                responseObject.statusCode = 400;
+                responseObject.statusDescription = "Invalid character ID";
+                return Ok(responseObject);
             }
 
-            return character;
+            return characterList;
         }
 
         // GET: api/House
         [HttpGet("/api/house")]
         public async Task<ActionResult<IEnumerable<House>>> GetHouse()
         {
-            //if (_context.Houses == null)
-            //{
-            //    return NotFound();
-            //}
             return Ok(await _context.House.ToListAsync());
         }
 
@@ -97,35 +89,44 @@ namespace MyFirstAPI.Controllers
         [HttpPost("/api/character")] //post
         public async Task<ActionResult<Character>> PostCharacter(Character character)
         {
-            if (_context.Characters == null)
+            Response responseObject = new Response();
+            var findChar = await _context.Characters.FindAsync(character.CharacterID);
+
+            if (findChar != null)
             {
-                return Problem("Entity set 'MyFirstAPIDBContext.Characters'  is null.");
+                responseObject.statusCode = 400;
+                responseObject.statusDescription = "Character cannot be created, existing ID.";
+
+                return Ok(responseObject);
             }
+
             _context.Characters.Add(character);
             await _context.SaveChangesAsync();
-
-            //return CreatedAtAction("GetCharacter", new { id = character.CharacterID }, character);
-            return Ok(await _context.Characters.ToListAsync());
+            responseObject.statusCode = 200;
+            responseObject.statusDescription = "Character added created";
+            return Ok(responseObject);
         }
 
         // DELETE: api/Character/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCharacter(int id)
+        public async Task<Response> DeleteCharacter(int id)
         {
-            if (_context.Characters == null)
-            {
-                return NotFound();
-            }
+            Response responseObject = new Response();
+
             var character = await _context.Characters.FindAsync(id);
             if (character == null)
             {
-                return NotFound();
+                responseObject.statusCode = 400;
+                responseObject.statusDescription = "Invalid character ID";
+                return responseObject;
             }
 
-            _context.Characters.Remove(character);
+            _context.Characters.Remove(character);  
             await _context.SaveChangesAsync();
+            responseObject.statusCode = 200;
+            responseObject.statusDescription = "Character deleted";
 
-            return NoContent();
+            return responseObject;
         }
 
         private bool CharacterExists(int id)
@@ -136,129 +137,3 @@ namespace MyFirstAPI.Controllers
 }
 
 
-
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using MyFirstAPI.Models;
-
-//namespace MyFirstAPI.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class CustomerController : ControllerBase
-//    {
-//        private readonly MyFirstAPIDBContext _context;
-
-//        public CustomerController(MyFirstAPIDBContext context)
-//        {
-//            _context = context;
-//        }
-
-//        // GET: api/Customer
-//        [HttpGet("/api/customer")]
-//        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
-//        {
-//          if (_context.Customers == null)
-//          {
-//              return NotFound();
-//          }
-//            return await _context.Customers.ToListAsync();
-//        }
-
-//        // GET: api/Customer/5
-//        [HttpGet("{id}")]
-//        public async Task<ActionResult<Customer>> GetCustomer(int id)
-//        {
-//          if (_context.Customers == null)
-//          {
-//              return NotFound();
-//          }
-//            var customer = await _context.Customers.FindAsync(id);
-
-//            if (customer == null)
-//            {
-//                return NotFound();
-//            }
-
-//            return customer;
-//        }
-
-//        // PUT: api/Customer/5
-//        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-//        [HttpPut("{id}")]
-//        public async Task<IActionResult> PutCustomer(int id, Customer customer)
-//        {
-//            if (id != customer.CustomerID)
-//            {
-//                return BadRequest();
-//            }
-
-//            _context.Entry(customer).State = EntityState.Modified;
-
-//            try
-//            {
-//                await _context.SaveChangesAsync();
-//            }
-//            catch (DbUpdateConcurrencyException)
-//            {
-//                if (!CustomerExists(id))
-//                {
-//                    return NotFound();
-//                }
-//                else
-//                {
-//                    throw;
-//                }
-//            }
-
-//            return NoContent();
-//        }
-
-//        // POST: api/Customer
-//        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-//        [HttpPost("/api/customer")]
-//        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
-//        {
-//          if (_context.Customers == null)
-//          {
-//              return Problem("Entity set 'MyFirstAPIDBContext.Customers'  is null.");
-//          }
-//            _context.Customers.Add(customer);
-//            await _context.SaveChangesAsync();
-
-//            return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
-//        }
-
-//        // DELETE: api/Customer/5
-//        [HttpDelete("{id}")]
-//        public async Task<IActionResult> DeleteCustomer(int id)
-//        {
-//            //from customers, grab email from customer, and only take the customer that matches == id
-//            var customer = await _context.Customers.Include(c => c.Email).FirstOrDefaultAsync(c => c.CustomerID == id);
-//            if (customer == null)
-//            {
-//                return NotFound();
-//            }
-
-//            _context.Customers.Remove(customer);
-
-//            var email = await _context.Emails.FirstOrDefaultAsync(e => e.EmailId == customer.Email.EmailId);
-
-//            _context.Emails.Remove(email);
-
-//            await _context.SaveChangesAsync();
-
-//            return NoContent();
-//        }
-
-//        private bool CustomerExists(int id)
-//        {
-//            return (_context.Customers?.Any(e => e.CustomerID == id)).GetValueOrDefault();
-//        }
-//    }
-//}
